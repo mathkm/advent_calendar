11436c425b2a8bc3e299f08e6a37115f181603db
松尾　航佑 <matsuo-k@illmatics.co.jp>
2017-07-18 19:13:38 +0900

    カレンダー表示用のモデルが大方出来上がった気がします。

--- src/main/java/com/example/domain/CalendarDay.java
+++ src/main/java/com/example/domain/CalendarDay.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -21,6 +22,9 @@ import com.example.service.AdventCalendarService;
 import com.example.service.ArticleService;
 import com.example.service.ThemeService;
 
+//全体的に改善できそう。
+//コンストラクタの中に書かなくていいものとかたくさんあると思います。
+
 public class CalendarDay{
 	
 	// カレンダーの年月日
@@ -33,7 +37,7 @@ public class CalendarDay{
 	ArticleRepository articleRepository;
 	// Theme型定義
 	Theme theme;
-	// DBからStringで取得したものをどうにかするんだよ？
+	// DBからStringで取得したものをどうにかする用
 	String inlineStrEnabledDates;
 	String[] strEnabledDates;
 	int[] enabledDates;
@@ -52,6 +56,7 @@ public class CalendarDay{
 	SimpleDateFormat dd;
 	SimpleDateFormat yyyymm;
 	
+	//コンストラクタで値を初期化しまくる。
 	public CalendarDay(Date calendarMonth,Calendar calendarDate,
 			ThemeRepository themeRepository,ArticleRepository articleRepository){
 		this.calendarMonth = calendarMonth;
@@ -63,22 +68,24 @@ public class CalendarDay{
 		// 同じ年月なのか確認するためyyyy-MMにするフォーマッター
 		yyyymm = new SimpleDateFormat("yyyyMM");
 		// 記事の日付をDate型で取り出す
-		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("JST"));
-		cal.setTime(calendarMonth);
-		cal.set(Calendar.HOUR_OF_DAY, 0);
-		cal.set(Calendar.MINUTE, 0);
-		cal.set(Calendar.SECOND, 0);
-		cal.set(Calendar.MILLISECOND, 0);
-		sqlCalendarMonth = new java.sql.Date(cal.getTimeInMillis());
     	articleDate = calendarDate.getTime();
+    	//SQLに送る用の形にカレンダーの月と記事の日付を変換する。
+    	Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("JST"));
 		cal.setTime(articleDate);
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
 		cal.set(Calendar.MILLISECOND, 0);
 		sqlArticleDate = new java.sql.Date(cal.getTimeInMillis());
+		cal.setTime(calendarMonth);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		sqlCalendarMonth = new java.sql.Date(cal.getTimeInMillis());
+		//各メソッドに値を渡していく感じで
 		getArticle(sqlArticleDate,articleRepository);
-		isAvalable(sqlArticleDate,yyyymm);
+		isAvalable(sqlArticleDate,sqlCalendarMonth,yyyymm);
 		isEnabled(sqlCalendarMonth,sqlArticleDate,themeRepository,dd);
 		isRegistered(sqlArticleDate,articleRepository);
 	}
@@ -94,8 +101,8 @@ public class CalendarDay{
 	}
 
 	// テーマと同じ年・月であればtrue
-	public boolean isAvalable(java.sql.Date sqlArticleDate,SimpleDateFormat yyyymm) {
-		strThemeYearMonth = yyyymm.format(calendarMonth);
+	public boolean isAvalable(java.sql.Date sqlArticleDate,java.sql.Date sqlcalendarMonth,SimpleDateFormat yyyymm) {
+		strThemeYearMonth = yyyymm.format(sqlcalendarMonth);
 		strArticleYearMonth = yyyymm.format(sqlArticleDate);
 		themeYearMonth = Integer.parseInt(strThemeYearMonth);
 		articleYearMonth = Integer.parseInt(strArticleYearMonth);
@@ -109,22 +116,33 @@ public class CalendarDay{
 	// テーマで許可されている日に存在していればtrue
 	public boolean isEnabled(java.sql.Date sqlCalendarMonth,java.sql.Date sqlArticleDate,ThemeRepository themeRepository,SimpleDateFormat dd) {
 		theme = themeRepository.findByCalendarMonth(sqlCalendarMonth);
+		if(theme == null){
+			return false;
+		}
 		inlineStrEnabledDates = theme.getEnabledDates();
+		//[1,2,3]みたいな形で取れるようになっているので、カンマで区切って配列に変える。
 		strEnabledDates = inlineStrEnabledDates.split(",");
 		strArticleDay = dd.format(sqlArticleDate);
 		articleDay = Integer.parseInt(strArticleDay);
 		enabledDates = new int[strEnabledDates.length];
 		
+		//現在Stringの配列なのでint[]に変換
 		for (int i = 0; i < strEnabledDates.length; i++) {
 			try{
 		       enabledDates[i] = Integer.parseInt(strEnabledDates[i]);
 			}catch (NumberFormatException e) {
-		           e.printStackTrace(); // -5aのとき、スタックトレースが出力される
+		           e.printStackTrace();
 		       }
 		    }
 		
-		List<int[]> list = Arrays.asList(enabledDates);
+		//int型の値検索をするためにInteger型のListに変換する
+		List<Integer> list = new ArrayList<Integer>(enabledDates.length);
+		for (int i = 0; i < enabledDates.length; i++) {
+		    list.add(enabledDates[i]);
+		}
 		
+		// なぜなら、Arrays.asListはオブジェクト型じゃないとリスト化してくれないため。
+		// listの中身とcontainsで調べる値の型は同じでないとfalseになる。
 		if (list.contains(articleDay) == true) {
 			return true;
 		} else {
--- src/main/java/com/example/service/AdventCalendarService.java
+++ src/main/java/com/example/service/AdventCalendarService.java
@@ -31,61 +31,98 @@ public class AdventCalendarService {
 	@Autowired
 	ArticleRepository articleRepository;
 	
+	static Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("JST"));
+		
 	public List<CalendarDay> generateCalendarDays() {
+		
+		List<CalendarDay> list = new ArrayList<CalendarDay>();
 
-		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("JST"));
 		int year = cal.get(Calendar.YEAR);
 		int month = cal.get(Calendar.MONTH);
     	cal.set(year,month,1,0,0,0);
-    	Date calendarMonth = cal.getTime();
-    	
-		List<CalendarDay> list = new ArrayList<CalendarDay>();
+	   	Date calendarMonth = cal.getTime();
 		
-		CalendarDay calendarDay = new CalendarDay(calendarMonth,cal,themeRepository,articleRepository);
-		
-		int subDay = cal.get(Calendar.DAY_OF_WEEK) - 7;
-        if (subDay < 0) {
+	   	// カレンダーの一番左上までカレンダーを引き戻す
+	   	int firstweek = cal.get(Calendar.DAY_OF_WEEK);
+	   	int subDay = 0;
+	   	if(firstweek == 1){
+	   	}else if(firstweek >= 2){
+	   		
+	   		while(firstweek > 1){
+	   			subDay--;
+	   			firstweek--;
+	   		}
+	   		
+	   	}
+	   	
+	   	cal.add(Calendar.DAY_OF_MONTH, subDay);
 
-            cal.add(Calendar.DAY_OF_MONTH, subDay);
-
-        }
-
+	   	//whileコントロール用のフラグ
         boolean flag = true;
-        //ここもたぶんおかしいです
-        while(flag == true){
-        	for(int i = 1 ; i == 7 ; i++){
+        
+        while(flag){
+        	//一週間ごとにコンストラクタを呼び起こして、Listに足していく
+        	for(int i = 0 ; i <= 6 ; i++){
         		
-        		cal.add(Calendar.DAY_OF_MONTH, i);
-        		list.add(calendarDay);
+        		if(list.size() == 0){
+        		
+        			CalendarDay calendarDay = new CalendarDay(calendarMonth,cal,themeRepository,articleRepository);
+        			list.add(calendarDay);
+        			
+        		}else{
+        			
+        			cal.add(Calendar.DAY_OF_MONTH, 1);
+        			CalendarDay calendarDay = new CalendarDay(calendarMonth,cal,themeRepository,articleRepository);
+        			list.add(calendarDay);
+        		
+        		}
         		
         	}
         	
+        	//35番目の部分になったら終了させていい可能性が出るので、チェックを行う。
         	if(list.size() == 35){
         		
+        		//35番目の日付
         		int lastday = cal.get(Calendar.DAY_OF_MONTH);
+        		//その時の月
         		int checkmonth = cal.get(Calendar.MONTH) + 1;
-        		int[] monthlist = {1,3,5,7,8,10,12};
-        		int[] monthlist2 = {2,4,6,9,11};
-        			
+        		//31日がある月
+        		int[] intmonthlist = {1,3,5,7,8,10,12};
+        		//30日までの月
+        		int[] intmonthlist2 = {2,4,6,9,11};
+        		
+        		// 値の中を調べるためにIntegerのListに変換する
+        		List<Integer> monthlist = new ArrayList<Integer>(intmonthlist.length);
+        		for (int j = 0; j < intmonthlist.length; j++) {
+        		    monthlist.add(intmonthlist[j]);
+        		}
+        		
+        		List<Integer> monthlist2 = new ArrayList<Integer>(intmonthlist2.length);
+        		for (int k = 0; k < intmonthlist2.length; k++) {
+        		    monthlist2.add(intmonthlist2[k]);
+        		}
+        			//lastdayが29日で2月でなければtrueのまま。
         			if (lastday == 29 && checkmonth != 2) {
         			
         				flag = true;
-        				
+        			//lastdayが30日で、31日がある月であればtrueのまま。	
         			} else if (lastday == 30 &&
-        					Arrays.asList(monthlist).contains(checkmonth) ){
+        					monthlist.contains(checkmonth) ){
         				
         				flag = true;
-        			
+        			//lastdayが29日で2月であればfalse。
         			} else if (lastday == 29 && checkmonth == 2){
         				
         				flag = false;
         				
+        			//lastdayが30日で、31日がある月でなければfalse。
         			} else if (lastday == 30 &&
-        					Arrays.asList(monthlist2).contains(checkmonth)){
+        					monthlist2.contains(checkmonth)){
         				
         				flag = false;
         				
-        			} else if (lastday != 29 || lastday != 30 && lastday < 7) {
+        			//lastdayが少ない数字（いちお7未満とかで）か、31日であればfalse。
+        			} else if (lastday != 29 || lastday != 30 && lastday < 7 || lastday == 31) {
         				
         				flag = false;
         				
@@ -105,4 +142,12 @@ public class AdventCalendarService {
 		
 	}
 	
+	// 変えれるか知らんけど書いとく
+	public void addMonth(){
+		cal.add(Calendar.MONTH,+1);
+	}
+
+	public void minMonth(){
+		cal.add(Calendar.MONTH,-1);
+	}
 }
